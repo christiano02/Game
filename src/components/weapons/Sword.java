2892cbafabb5f092c3997c1b8d2ba5eb35e0ee96
Christiano <christianonascimento02@hotmail.com>
2025-05-17 22:31:45 -0300

    criando uma base inicial para armas

--- src/components/weapons/Sword.java
+++ src/components/weapons/Sword.java
@@ -1,42 +1,16 @@
 package components.weapons;
 
-public class Sword {
-    private int base_damage;
-    private String sword_name;
-    private int level;
-    public Sword(int base_damage, String sword_name){
-        this.base_damage = base_damage;
-        this.sword_name = sword_name;
+public class Sword extends Weapons {
+
+    public Sword() {
     }
 
-    public int getBase_damage() {
-        return base_damage;
+    public Sword(int baseDamage, int level, String swordName) {
+        super(baseDamage, level, swordName);
     }
 
-    public void setBase_damage(int base_damage) {
-        this.base_damage = base_damage;
-    }
-
-    public String getSword_name() {
-        return sword_name;
-    }
-
-    public void setSword_name(String sword_name) {
-        this.sword_name = sword_name;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-    public void level_up(){
-        if (getLevel() == 2){
-
-        }
+    @Override
+    void upLevelArm() {
 
     }
-
 }
---
+++ src/components/weapons/Weapons.java
@@ -0,0 +1,42 @@
+package components.weapons;
+
+public abstract class Weapons {
+    private int baseDamage;
+    private String swordName;
+    private int level;
+
+    public Weapons() {
+    }
+
+    public Weapons(int baseDamage, int level, String swordName) {
+        this.baseDamage = baseDamage;
+        this.level = level;
+        this.swordName = swordName;
+    }
+
+    public int getBaseDamage() {
+        return baseDamage;
+    }
+
+    public void setBaseDamage(int baseDamage) {
+        this.baseDamage = baseDamage;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getSwordName() {
+        return swordName;
+    }
+
+    public void setSwordName(String swordName) {
+        this.swordName = swordName;
+    }
+    //metodo de nivel da arma
+    abstract void upLevelArm();
+}
