a1be3e78074befc03418df3b88a061d34b75adef
Christiano <christianonascimento02@hotmail.com>
2025-05-25 21:26:25 -0300

    Adicionando uma função de cura na classe Item

--- src/components/logic/battleSystem/itens/Item.java
+++ src/components/logic/battleSystem/itens/Item.java
@@ -1,6 +1,29 @@
 package components.logic.battleSystem.itens;
 
+import components.character.Player;
+
+import java.util.Objects;
+
 public class Item {
     private String name;
     private String type;
+    private Player player;
+    public Item(){
+    }
+
+    public Item(String name, String type, Player player){
+        this.name = name;
+        this.type = type;
+        this.player = player;
+    }
+
+    public boolean UseItem(Player player){
+        System.out.println("Vida atual: " + player.getLife());
+        if(Objects.equals(this.type, "heal")){
+            player.setLife(player.getLife() + 10);
+            System.out.println("+ " + 10 + "de vida.");
+            System.out.println("Vida atual: " + player.getLife());
+        }
+        return true;
+    }
 }
